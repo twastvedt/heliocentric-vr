c7961534b208d313d6d8d29d3d81bfb2deb14524
Trygve Wastvedt <twastvedt@azahner.com>
2018-01-05 21:34:32 -0600

    Fix paths, add hemisphere starting location.

--- src/systems/HemStepsMat.system.ts
+++ src/systems/HemStepsMat.system.ts
@@ -36,9 +36,9 @@ export const HemStepsMatSys: AFrame.SystemDefinition<HemStepsMatSysOb> = {
     this.sunSystem = <any>document.querySelector('a-scene').systems['sun-system'] as sunSystemOb;
 
     const loader = new AFRAME.THREE.TextureLoader();
-    const concreteTexture = loader.load( '../assets/hemisphere/concrete-19-2048.png' );
+    const concreteTexture = loader.load( './assets/hemisphere/concrete-19-2048.png' );
     // const concreteTexture = loader.load( '../assets/hemisphere/uv_checker_large.png' );
-    const lightMap = loader.load( '../assets/hemisphere/lightMap.png' );
+    const lightMap = loader.load( './assets/hemisphere/lightMap.png' );
 
     //UVs are getting flipped in the blender export. flipY defaults to true, but setting it to false fixes this.
     lightMap.flipY = false;
